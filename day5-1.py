51faed84f4d56fe3455a6568bdadbc9b16196175
Derek Elliott <saywhat1@gmail.com>
2016-12-18 01:52:18 -0500

    Add day 5 part 1.

---
+++ day5-1.py
@@ -0,0 +1,23 @@
+"""Module to find the passowrd on a bunny door."""
+
+
+import hashlib
+
+
+def main():
+    """Run the main function."""
+    id = 'cxdnnyjw'
+    password = []
+    begin = '00000'
+    index = 0
+
+    while len(password) < 8:
+        test = id + str(index)
+        if begin == hashlib.md5(test).hexdigest()[0:5]:
+            password.append(hashlib.md5(test).hexdigest()[5])
+        index += 1
+    print('The password is {}').format(''.join(password))
+
+
+if __name__ == '__main__':
+    main()
